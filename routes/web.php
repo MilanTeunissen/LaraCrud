94585a80f43aa10994fb208ca7a60afdc60afc47
Milan <milanteunissen2003@gmail.com>
2025-06-02 19:23:21 +0200

    Added comments and made middleware to check for admin perms

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,32 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-// Public Routes
+//view single product
 Route::get('/products/{product}/view-single', [\App\Http\Controllers\productController::class, 'view'])->name('products.viewSingle');
+
+// Remove 1 product from stock
 Route::post('/products/{product}/decrement-stock', [\App\Http\Controllers\productController::class, 'decrementStock'])->name('products.decrementStock');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
+    // Users on dashboard page
     Route::get('dashboard', [\App\Http\Controllers\Auth\RegisteredUserController::class, 'index'])->name('dashboard');
+
+    // Change user rank
     Route::put('/users/{user}/rank', [\App\Http\Controllers\Auth\RegisteredUserController::class, 'updateRank'])->name('users.updateRank');
 
+    // Products management page
     Route::get('/products', [\App\Http\Controllers\productController::class, 'index'])->name('products.index');
+
+    // Create products
     Route::get('/products/create', [\App\Http\Controllers\productController::class, 'create'])->name('products.create');
     Route::post('/products', [\App\Http\Controllers\productController::class, 'store'])->name('products.store');
+
+    // Edit/Update products
     Route::get('/products/{product}/edit', [\App\Http\Controllers\productController::class, 'edit'])->name('products.edit');
     Route::put('/products/{product}', [\App\Http\Controllers\productController::class, 'update'])->name('products.update');
+
+    // Delete products
     Route::delete('/products/{product}', [\App\Http\Controllers\productController::class, 'destroy'])->name('products.destroy');
 
 });
